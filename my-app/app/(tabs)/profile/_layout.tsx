9e7f3b16e0d9dd27cc7655c8a93700c5d63e2853
x-lvr <xlavreniuk@gmail.com>
2026-02-02 02:10:06 +0200

    !need to fix top margin

--- my-app/app/(tabs)/profile/_layout.tsx
+++ my-app/app/(tabs)/profile/_layout.tsx
@@ -2,16 +2,13 @@ import { Stack } from 'expo-router';
 
 export default function ProfileStackLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerLargeTitle: false,
-      }}>
-      <Stack.Screen name="index" options={{ title: 'Profile' }} />
-      <Stack.Screen name="edit" options={{ title: 'Edit Profile' }} />
-      <Stack.Screen name="account-identity" options={{ title: 'Account & Identity' }} />
-      <Stack.Screen name="security-privacy" options={{ title: 'Security & Privacy' }} />
-      <Stack.Screen name="devices-sessions" options={{ title: 'Devices & Sessions' }} />
-      <Stack.Screen name="data-activity" options={{ title: 'Data & Activity' }} />
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="edit" />
+      <Stack.Screen name="account-identity" />
+      <Stack.Screen name="security-privacy" />
+      <Stack.Screen name="devices-sessions" />
+      <Stack.Screen name="data-activity" />
     </Stack>
   );
 }
--- my-app/app/(tabs)/settings/_layout.tsx
+++ my-app/app/(tabs)/settings/_layout.tsx
@@ -1,5 +1,5 @@
 import { Stack } from 'expo-router';
 
 export default function SettingsStackLayout() {
-  return <Stack />;
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
